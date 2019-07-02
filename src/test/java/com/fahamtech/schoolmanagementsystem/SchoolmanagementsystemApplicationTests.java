9befdc9364096361332fe8c567116a63199ff501
tiffahm <tiffahkapiligi@gmail.com>
2019-07-02 18:56:55 +0100

    refresh

---
+++ src/test/java/com/fahamtech/schoolmanagementsystem/SchoolmanagementsystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fahamtech.schoolmanagementsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SchoolmanagementsystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
